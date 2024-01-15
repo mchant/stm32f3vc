37d82fff216252598f23b64efd0c1c5587abc58b
peakgradient <peakgradient@flexbook2.peakgradient.com>
2024-01-15 08:44:54 -0800

    added usb-serial

---
+++ embassy/helloworld2/flash_release.sh
@@ -0,0 +1,1 @@
+probe-rs run --chip stm32f303vct6 ./target/thumbv7em-none-eabihf/release/helloworld2
--- embassy/rust-stm32f3-embassy-helloworld2/flash_release.sh
+++
@@ -1,1 +0,0 @@
-probe-rs run --chip stm32f303vct6 ./target/thumbv7em-none-eabihf/release/rust-stm32f3-embassy-helloworld2
